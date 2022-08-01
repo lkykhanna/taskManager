9332c760fca49a05040610df50fd509099e0a049
Lucky <luckykhanna09@gmail.com>
2022-08-01 13:21:04 +0530

    Change port number in app.js file

--- app.js
+++ app.js
@@ -163,11 +163,11 @@ app.get("/about", function(req, res) {
 });
 
 let port = process.env.PORT;
-if (port == null || PORT == "") {
+if (port == null || port == "") {
     port = 3000;
 }
 
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(port, function() {
+    console.log("Server started successfully");
 });
