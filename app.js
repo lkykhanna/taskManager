30c1c8eb336b07cda744bdc845e2976c9fc044fc
Lucky <luckykhanna09@gmail.com>
2022-08-01 13:10:00 +0530

    Add new files giotignore and procfile

--- app.js
+++ app.js
@@ -162,6 +162,12 @@ app.get("/about", function(req, res) {
     res.render("about");
 });
 
+let port = process.env.PORT;
+if (port == null || PORT == "") {
+    port = 3000;
+}
+
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
